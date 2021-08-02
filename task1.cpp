baf4c35d696364d6880677b57fd96eae11d40a44
Glink31 <Goshazherebtsov@gmail.com>
2021-08-02 16:03:31 +0300

    first commit

---
+++ task1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    int a,b,c ;
+    cin >> a >> b >> c;
+    if (a + b > c) and (b + c > a) and (c + a > b);
+        cout >> "the triangle exists";
+    else
+        cout >> "the triangle does not exist"
+}
