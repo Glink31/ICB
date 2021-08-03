a0723b01500117ba07e970b55a0c8a475dd7cd02
g.zherebtsov <56264576+Glink31@users.noreply.github.com>
2021-08-03 17:25:44 +0500

    Add files via upload

--- firec.cpp
+++ firec.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
+#include <chrono>
+
+int fib(int n){
+	if (n == 1 || n == 2){
+		return 1;
+	}else{
+		return fib(n-1)+fib(n-2);
+	}
+}
 
 int main() {
 	auto t1 = std::chrono::high_resolution_clock::now();
-    int a, b = 1, c = 1;
+    int a,b;
 	std::cin >> a;
-	for(int i = 2;i < a;i++){
-		b = b + c;
-		c = b - c;
-	}
+	b = fib(a);
 	std::cout << "В ряде Фибаначи на позиции " << a << " стоит число " << b << "\n";
 	auto t2 = std::chrono::high_resolution_clock::now();
 	auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
 	std::cout << "Time taken to run the program " << duration << " nanoseconds\n";
-	return 0
+	return 0;
 }
